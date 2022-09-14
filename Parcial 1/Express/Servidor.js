18cfc89100161aa770db8b8bae6991d1a1cc984c
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-09-14 08:41:43 -0500

    Listo

--- Parcial 1/Express/Servidor.js
+++ Parcial 1/Express/Servidor.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
+app.use(cors({ origin: "http://localhost:8082"}))
 
 app.get('/',(req,res)=>{
     res.send('Servidor express en funcion')

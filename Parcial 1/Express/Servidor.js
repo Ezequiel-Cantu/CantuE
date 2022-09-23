277396a463a4d947fbc7292a7a962552608e47e1
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-09-22 20:17:19 -0500

    Listo

--- Parcial 1/Express/Servidor.js
+++ Parcial 1/Express/Servidor.js
@@ -1,23 +1,56 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { text } = require('express');
 const app = express();
 
 app.use(cors({ origin: "http://localhost:8080"}))
 
+//Middleware
+app.use(express.text())
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     //res.send('Servidor express en funcion')
     res.sendFile('./Static/Index.html',{root: __dirname}, (err) => {console.log('Achivo enviado correctamente')})
-});
-app.post('/',(req,res)=>{
-    res.json({usuario: 'Ezequiel'})
-});
+})
+
+
+// Procesamiento de Texto
+app.post('/texto',(req,res)=>{
+    console.log(req.body)
+    let mayus = req.body.toUpperCase()    
+    let sinEspacio = req.body.trim()
+    let longit = req.body.lenght
+    res.json({mayusculas: mayus,
+              NoEspacios: sinEspacio,
+              longitud: longit})
+})
+
+// Tener un Json
+app.post('/json',(req,res) =>{
+    console.log(req.body.nombre)
+    let cadena = "Bienvenido " +req.body.nombre+ " " +req.body.apellido+ ", este es un json"
+    res.json({saludo:cadena})
+})
+
+app.get('/mayusculas/:cadena',(req,res) =>{
+    console.log(req.params)
+    res.send(req.params)
+})
+
+app.get('/suma',(req,res) => {
+    console.log(req.query)
+    let suma = parseInt(req.query.a) + parseInt(req.query.b)
+    res.send(`El resultado es ${suma}`)
+})
+
 
 app.use((req,res) => {
     res.status(404).sendFile('./Static/404.html',{root: __dirname})
-});
+})
 
 app.listen(8082,(req,res) => {
     console.log('Server escuchando por puerto 8082')
     console.log(__dirname)
     console.log(__filename)
-});
+})

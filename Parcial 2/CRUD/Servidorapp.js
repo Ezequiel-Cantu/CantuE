e729531fdb70de01633fb9959e71839186577613
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-10-22 16:30:03 -0500

    CRUD

--- Parcial 2/CRUD/Servidorapp.js
+++ Parcial 2/CRUD/Servidorapp.js
@@ -1,35 +1,43 @@
 const express = require('express');
-let mysql = require('mysql')
-let json2xls  = require('json2xls')
-let fs = require('fs')
+const mysql2 = require('mysql2/promise');
 
-var con = mysql.createConnection({
+
+const pool = mysql2.createPool({
     host : 'localhost',
     user : 'root',
+    port: '3306',
     password : '',
     database : 'l19100155'
 });
 
-const app = express()
+const app = express();
 
 app.use(express.text())
 app.use(express.json())
 
-app.get('/usuario/:idUsuario',(req,res) =>{
-    
-    con.connect();
-    
-    con.query('SELECT * FROM usuario WHERE idUsuario =' +req.params.idUsuario+ ';', function(error, results, fields) {
-        console.log(results);
-        res.json(results)
-    });
-    
-    con.end();
+app.get('/usuario/:idUsuario', async(req,res) =>{
+    const ID = req.params.idUsuario;
+    const [responseBD] = await pool.execute(`SELECT * FROM usuario WHERE idUsuario=${ID}`);
+    res.json(responseBD);
+});
 
-})
+app.post('/registroUsuario', async (req,res) => {
+    const [responseBD] = await pool.execute(`INSERT INTO usuario(Nombre, ApPat, Edad, Ocupacion, Usuario, Telefono, Genero, Contrasena) VALUES ('${req.body.Nombre}','${req.body.ApPat}','${req.body.Edad}','${req.body.Ocupacion}','${req.body.Usuario}','${req.body.Telefono}','${req.body.Genero}','${req.body.Contrasena}')`);
+    res.send(responseBD);
+});
+app.put('/editarUsuario/:idUsuario', async (req,res) => {
+    const ID = req.params.idUsuario;
+    const [responseBD] = await pool.execute(`UPDATE usuario SET Nombre='${req.body.Nombre}',ApPat='${req.body.ApPat}',Edad='${req.body.Edad}',Ocupacion='${req.body.Ocupacion}',Usuario='${req.body.Usuario}',Telefono='${req.body.Telefono}',Genero='${req.body.Genero}',Contrasena='${req.body.Contrasena}' WHERE idUsuario=${ID}`);
+    res.send(responseBD);
+});
+app.delete('/borrarUsuario/:idUsuario', async (req,res) => {
+    const ID = req.params.idUsuario;
+    const [responseBD] = await pool.execute(`DELETE FROM usuario WHERE idUsuario=${ID}`);
+    res.json(responseBD);
+});
 
 app.listen(8082,(req,res) => {
-    console.log('Server escuchando por puerto 8082')
-})
+    console.log('Este server esta escuchando por puerto 8082');
+});
 
 

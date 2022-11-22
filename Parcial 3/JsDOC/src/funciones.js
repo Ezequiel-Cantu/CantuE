a36939d39aa72ea0c6c15f765d7e64bd7df0bcdd
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2022-11-22 08:21:42 -0600

    jsdoc

--- Parcial 3/JsDOC/src/funciones.js
+++ Parcial 3/JsDOC/src/funciones.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @param {Number} numerobase Numero a elevar
+ * @param {Number} exponente  Potencia que elevara
+ * @returns  Number
+ */
+
 export function potencia(numerobase,exponente){
     return numerobase*exponente;
 }
